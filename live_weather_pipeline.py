f30cf10bd25de14519c083f98e095cf1fcc94fb4
preritkandpal12-dotcom <preritkandpal12@gmail.com>
2026-06-13 22:35:03 +0530

    Update weather pipeline with interactive inputs
    Replaced hardcoded Mumbai coordinates with an interactive terminal loop to fetch live Open-Meteo API data for multiple cities.

--- live_weather_pipeline.py
+++ live_weather_pipeline.py
@@ -1,11 +1,25 @@
 import requests
-url = url = "https://api.open-meteo.com/v1/forecast?latitude=16.6568&longitude=73.517&current_weather=true"
-print("Sending HTTP GET Request to Open-Meteo Servers...")
+CITIES = {
+    "mumbai": {"lat": 19.0760, "lon": 72.877},
+    "london": {"lat": 51.5074, "lon": -0.1278},
+    "new york": {"lat": 40.7128, "lon": -74.0060}
+}
+
+user_choice = input("Enter a city (Mumbai, London, New york): ").strip().lower() 
+
+if user_choice in CITIES:
+    coords = CITIES[user_choice]
+
+url = f"https://api.open-meteo.com/v1/forecast?latitude={coords['lat']}&longitude={coords['lon']}&current_weather=true"
+
+print(f"\nConnecting to weather grid for {user_choice.title()}...")
 response = requests.get(url)
+
 print(f"Server response code: {response.status_code}")
 if response.status_code == 200:
     data = response.json()
     current = data['current_weather']
+
     print(f"Current Temperature: {current['temperature']}°C")
     print(f"Wind Speed: {current['windspeed']} km/hr")
 else:
