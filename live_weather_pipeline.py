f613b1a3be773f24ced8e68d2585cf4fc8a01a9d
preritkandpal12-dotcom <preritkandpal12@gmail.com>
2026-06-21 16:06:05 +0530

    Upgrade API pipeline with automated batch processing loop
    Replaced manual inputs with a Python dictionary loop to automatically fetch, format, and append live API data for multiple cities into the local CSV log in a single execution.

--- live_weather_pipeline.py
+++ live_weather_pipeline.py
@@ -1,42 +1,38 @@
 import requests
-import pandas as pd
+import pandas as pd 
 from datetime import datetime 
-CITIES = {
+
+CITIES = { 
     "mumbai": {"lat": 19.0760, "lon": 72.877},
     "london": {"lat": 51.5074, "lon": -0.1278},
     "new york": {"lat": 40.7128, "lon": -74.0060}
 }
+print("\n Initiating automated batch weather fetch...")
+for city_name, coords in CITIES.items():
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={coords['lat']}&longitude={coords['lon']}&current_weather=true"
 
-user_choice = input("Enter a city (Mumbai, London, New york): ").strip().lower() 
+    print(f"Connecting to grid for {city_name .title()}...")
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        current = data['current_weather']
 
-if user_choice in CITIES:
-    coords = CITIES[user_choice]
+        weather_dict = {
+            "Timestamp": [datetime.now().strftime("%Y-%m-%d %H:%M")],
+            "City": [city_name.title()],
+            "Temperature (c)": [current['temperature']],
+            "Wind speed (km/hr)": [current['windspeed']]
+        }
 
-url = f"https://api.open-meteo.com/v1/forecast?latitude={coords['lat']}&longitude={coords['lon']}&current_weather=true"
+        df = pd.DataFrame(weather_dict)
 
-print(f"\nConnecting to weather grid for {user_choice.title()}...")
-response = requests.get(url)
+        df.to_csv("my_weather_log.csv", mode = 'a', index=False, header=False)
+    else:
+        print(f"Failed to fetch data for {city_name.title() }")
+print("Batch process complete!\n")
 
-print(f"Server response code: {response.status_code}")
-if response.status_code == 200:
-    data = response.json()
-    current = data['current_weather']
 
-    print(f"Current Temperature: {current['temperature']}°C")
-    print(f"Wind Speed: {current['windspeed']} km/hr")
-    weather_dict = { 
-        "Timestamp": [datetime.now().strftime("%Y-%m-%d %H:%M")],
-        "City": [user_choice .title()],
-        "Temperature (C)": [current ['temperature']],
-        "Wind Speed (km/hr)": [current['windspeed']]
-        
-    }
 
-    df= pd.DataFrame(weather_dict)
-    df.to_csv("my_weather_log.csv" , mode='a' , index = False , header=False)
-    print("\n Live data succesfully saved to my_weather_log.csv ")
-else:
-    print("Network pipeline failed to fetch live data")
-    
 
-      
+         
+
