2affff2b4667afcdb6f9648b943ddc3990dbcb53
preritkandpal12-dotcom <preritkandpal12@gmail.com>
2026-06-23 17:29:22 +0530

    Refactor pipeline into modular functions
    Separated API fetching and Pandas CSV saving logic into distinct Python functions (Separation of Concerns). Added standard execution block for modularity and error handling.

--- live_weather_pipeline.py
+++ live_weather_pipeline.py
@@ -1,35 +1,48 @@
 import requests
-import pandas as pd 
-from datetime import datetime 
+import pandas as pd
+from datetime import datetime
 
-CITIES = { 
+
+CITIES = {
     "mumbai": {"lat": 19.0760, "lon": 72.877},
     "london": {"lat": 51.5074, "lon": -0.1278},
     "new york": {"lat": 40.7128, "lon": -74.0060}
 }
-print("\n Initiating automated batch weather fetch...")
-for city_name, coords in CITIES.items():
+
+def fetch_weather_data(city_name, coords):
     url = f"https://api.open-meteo.com/v1/forecast?latitude={coords['lat']}&longitude={coords['lon']}&current_weather=true"
-
-    print(f"Connecting to grid for {city_name .title()}...")
     response = requests.get(url)
+    
     if response.status_code == 200:
-        data = response.json()
-        current = data['current_weather']
-
+        current = response.json()['current_weather']
         weather_dict = {
             "Timestamp": [datetime.now().strftime("%Y-%m-%d %H:%M")],
             "City": [city_name.title()],
-            "Temperature (c)": [current['temperature']],
-            "Wind speed (km/hr)": [current['windspeed']]
+            "Temperature (C)": [current['temperature']],
+            "Wind Speed (km/hr)": [current['windspeed']]
         }
-
-        df = pd.DataFrame(weather_dict)
-
-        df.to_csv("my_weather_log.csv", mode = 'a', index=False, header=False)
+        return weather_dict
     else:
-        print(f"Failed to fetch data for {city_name.title() }")
-print("Batch process complete!\n")
+        print(f"Failed to fetch data for {city_name.title()}")
+        return None
+
+def save_to_csv(weather_dict):
+    df = pd.DataFrame(weather_dict)
+    df.to_csv("my_weather_log.csv", mode='a', index=False, header=False)
+    print(f" Saved data for {weather_dict['City'][0]}")
+
+def run_pipeline():
+    print("\n Initiating modular batch weather fetch...")
+    
+    for city_name, coords in CITIES.items():
+       weather_data = fetch_weather_data(city_name, coords)
+    if weather_data is not None:
+            save_to_csv(weather_data)
+            
+    print(" Pipeline execution complete!\n")
+
+if __name__ == "__main__":
+    run_pipeline()
 
 
 
